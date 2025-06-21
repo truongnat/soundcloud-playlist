0d42cbdf0dc64a06389333934c52f42db69996ff
truongnat <truongdq.dev@gmail.com>
2025-06-21 12:42:30 +0700

    feat: Change Cross-Origin-Embedder-Policy to credentialless

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
     routeRules: {
       '/**': {
         headers: {
-          'Cross-Origin-Embedder-Policy': 'require-corp',
+          'Cross-Origin-Embedder-Policy': 'credentialless',
           'Cross-Origin-Opener-Policy': 'same-origin'
         }
       }
