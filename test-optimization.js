8c46a574b1f32cc22ed911676cb33176acc8daae
truongnat <truongdq.dev@gmail.com>
2025-06-22 20:06:40 +0700

    feat: Migrate test script to ES modules

--- test-optimization.js
+++ test-optimization.js
@@ -5,8 +5,12 @@
  * Chạy: node test-optimization.js
  */
 
-const fs = require('fs')
-const path = require('path')
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 console.log('🚀 SoundCloud Playlist Download Speed Optimization Test\n')
 
